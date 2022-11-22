75c00df2dd569c85890a88b5e72a4d13b91c9ef2
Dvir Cohen <dvir906@gmail.com>
2022-11-22 22:05:46 +0200

    init & components &  routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Page404 from './components/Page404'
+import index from './pages/index'
+import DefaultLayout from './layout/DefaultLayout';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DefaultLayout>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<index />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
+    </DefaultLayout>
+  )
 }
 
-export default App;
+export default App
